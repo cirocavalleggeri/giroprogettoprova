077f9c1f6913140ecdd95955aa3f27f0273203a7
cirocavalleggeri <ciroallinone@gmail.com>
2021-12-09 16:13:20 +0100

    PRimo commit di prova per imparare git
    i primi 72 caratteri vengono presi in considerazione questo che scriv è considerato solo un commento

---
+++ app/src/main/java/com/example/ciro/terzaapp/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.ciro.terzaapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
