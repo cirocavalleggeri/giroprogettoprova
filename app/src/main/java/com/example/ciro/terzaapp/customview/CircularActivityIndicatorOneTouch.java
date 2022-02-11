e375dfbd4d7052acd545744f30c7f109122e8bc3
cirocavalleggeri <ciroallinone@gmail.com>
2022-02-11 20:51:08 +0100

    CustomCircularDrawindicatoroneThouch è stato aggiunto in tocco

---
+++ app/src/main/java/com/example/ciro/terzaapp/customview/CircularActivityIndicatorOneTouch.java
@@ -0,0 +1,114 @@
+package com.example.ciro.terzaapp.customview;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.graphics.Path;
+import android.graphics.Region;
+import android.os.Build;
+import android.support.annotation.RequiresApi;
+import android.util.AttributeSet;
+import android.util.Log;
+import android.view.MotionEvent;
+import android.view.View;
+
+public class CircularActivityIndicatorOneTouch extends View {
+    private static final int DEFAULT_FG_COLOR = 0xffff0000;
+    private static final int DEFAULT_BG_COLOR = 0xffa0a0a0;
+    private static final int PRESSED_FG_COLOR = 0xff0000ff;
+
+    private static final String TAG ="CIRC" ;
+    private Paint backgroundPaint;
+    private Paint foregroundPaint;
+    private int selectedAngle;
+    private Path clipPath;
+    Boolean pressed;
+    public CircularActivityIndicatorOneTouch(Context context, AttributeSet
+            attributeSet) {
+        super(context, attributeSet);
+        backgroundPaint = new Paint();
+        backgroundPaint.setColor(DEFAULT_BG_COLOR);
+        backgroundPaint.setStyle(Paint.Style.FILL);
+        foregroundPaint = new Paint();
+        foregroundPaint.setColor(DEFAULT_FG_COLOR);
+        foregroundPaint.setStyle(Paint.Style.FILL);
+        selectedAngle = 280;
+        pressed=false;
+    }
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
+    @Override
+    protected void onDraw(Canvas canvas) {
+        if (pressed) {
+            foregroundPaint.setColor(PRESSED_FG_COLOR);
+        } else {
+            foregroundPaint.setColor(DEFAULT_FG_COLOR);
+        }
+        int circleSize = getWidth();
+        if (getHeight() < circleSize) circleSize = getHeight();
+        int horMargin = (getWidth() - circleSize) / 2;
+        int verMargin = (getHeight() - circleSize) / 2;
+        // create a clipPath the first time
+        if(clipPath == null) {
+            int clipWidth = (int) (circleSize * 0.75);
+            int clipX = (getWidth() - clipWidth) / 2;
+            int clipY = (getHeight() - clipWidth) / 2;
+            clipPath = new Path();
+            clipPath.addArc(
+                    clipX,
+                    clipY,
+                    clipX + clipWidth,
+                    clipY + clipWidth,
+                    0, 360);
+        }
+        canvas.clipPath(clipPath, Region.Op.DIFFERENCE);
+        canvas.save();
+        canvas.rotate(-90, getWidth() / 2, getHeight() / 2);
+        canvas.drawArc(
+                horMargin,
+                verMargin,
+                horMargin + circleSize,
+                verMargin + circleSize,
+                0, 360, true, backgroundPaint);
+        canvas.drawArc(
+                horMargin,
+                verMargin,
+                horMargin + circleSize,
+                verMargin + circleSize,
+                0, selectedAngle, true, foregroundPaint);
+        canvas.restore();
+    }
+   /* @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        Log.d(TAG, "touch: " + event);
+        return super.onTouchEvent(event);
+    }*/
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        Log.d(TAG, "touch: " + event);
+        switch(event.getAction()) {
+            case MotionEvent.ACTION_DOWN:
+
+                pressed = true;
+                cambia_stato_pressed(pressed);
+                //invalidate() si usa quando l'aggiornamento viene richiesto dal Thread principale
+                // in caso contrario si usa postinvalidate();
+                invalidate();
+                //ritorniamo true quando vogliamo processare l'evento,ritorniamo  false quando
+                //non interessa e lasciamo alla finestra genitore il compito di elaborare l'evento
+                return true;
+            case MotionEvent.ACTION_UP:
+                pressed = false;
+                cambia_stato_pressed(pressed);
+                invalidate();
+                return true;
+            default:
+                return false;
+        }
+    }
+
+    private void cambia_stato_pressed(Boolean premuto) {
+        this.pressed=premuto;
+    }
+
+}
+
